088bb9160c4db350c2c3832f64110b07d6d5c1e9
4tochka <admin@bitaps.com>
2022-06-23 14:41:43 +0400

    v23.0

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build --no-cache -t  bitcoin-core .
-docker tag bitcoin-core bitaps/bitcoin-core:v0.20.0
+docker build  -t  bitcoin-core .
+docker tag bitcoin-core bitaps/bitcoin-core:v23.0
 docker tag bitcoin-core bitaps/bitcoin-core:latest
